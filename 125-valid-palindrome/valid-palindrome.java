332f33b27ac4f2ad8483fa7cf090c03d3ad814f1
NBN-Barath <1barathnivash@gmail.com>
2025-05-10 22:21:27 +0530

    Time: 13 ms (42.49%) | Memory: 45.4 MB (16.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,21 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         if (s.equals("")) {
-                return true;
-            }
-            s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) != s.charAt(s.length() - 1 - i)){
-                    return false;
-                }
-            }
             return true;
+        }
+
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", ""); 
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
         
