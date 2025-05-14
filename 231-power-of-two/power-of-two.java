d3cf40cb325e6ff1410de8c40f956c1a743b4ce0
NBN-Barath <1barathnivash@gmail.com>
2025-05-14 12:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (9.73%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -3,18 +3,9 @@ class Solution {
         if(n == 1){
             return true;
         }
-        if(n % 2 != 0){
+        if(n<=0){
             return false;
         }
-        return powerOfTwo(n,1);
-    }
-    public boolean powerOfTwo(int n, int pow){
-        if(Math.pow(2 , pow) == n){
-            return true;
-        }
-        if(Math.pow(2 , pow) > n){
-            return false;
-        }
-        return powerOfTwo(n,pow + 1);
+        return (n % 2 == 0) && isPowerOfTwo(n / 2);
     }
 }
