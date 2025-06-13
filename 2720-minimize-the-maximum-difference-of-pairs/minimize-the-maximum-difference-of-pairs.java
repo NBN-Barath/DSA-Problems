ef5ac40b247fe03a5c6ec66b2e0516ff1b7d75c2
NBN-Barath <1barathnivash@gmail.com>
2025-06-13 19:49:02 +0530

    Time: 16 ms (97.00%) | Memory: 56.5 MB (15.50%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int low = 0, high = nums[nums.length - 1] - nums[0];
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (canPair(nums, p, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private boolean canPair(int[] nums, int p, int maxDiff) {
+        int count = 0;
+        for (int i = 1; i < nums.length && count < p; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                count++;
+                i++; 
+            }
+        }
+        return count >= p;
+    }
+}
