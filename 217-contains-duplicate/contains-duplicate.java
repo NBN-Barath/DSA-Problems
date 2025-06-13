50df90fedc954691ea1f17f64c2905972b50de12
NBN-Barath <1barathnivash@gmail.com>
2025-06-13 11:19:29 +0530

    Time: 10 ms (92.65%) | Memory: 60.9 MB (12.71%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> hash = new HashSet<>();
         for(int i = 0; i < nums.length; i++){
-            if(hash.contains(nums[i])){
+            if(!hash.add(nums[i])){
                 return true;
             }
-            hash.add(nums[i]);
+            
         }
         return false;
     }
